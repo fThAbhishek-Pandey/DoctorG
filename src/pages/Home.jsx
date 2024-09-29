f6d34906d5aae3c78549069e120cc33b934799b1
fThAbhishek-Pandey <abhishek830564@email.com>
2024-09-29 15:48:19 +0530

    top docter UI creating

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
 import SpeciallityMenu from '../components/SpeciallityMenu'
+import TopDoctor from '../components/TopDoctor'
 const Home = () => {
   return (
     <div>Homepage
       <Header />
       <SpeciallityMenu/>
+      <TopDoctor />
     </div>
     
   )
