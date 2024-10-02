1e218d55c4f0c35aab65aec7e4f9922b6458b050
fThAbhishek-Pandey <abhishek830564@email.com>
2024-10-02 23:16:04 +0530

    push

--- src/components/Doctors/specialist.jsx
+++ src/components/Doctors/specialist.jsx
@@ -1,23 +1,27 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import { AppContext } from '../../context/AppContext'
 import { useNavigate } from 'react-router-dom';
 const Specialist = ({specialist}) => {
+    const [showFilter, setShowFilter] = useState(false);
     const {doctors} = useContext(AppContext);
     const navigate = useNavigate();
-    console.log(doctors);
+    // console.log(doctors);
     const DocSpecilistSet = new  Set(doctors.map ((item)=>{
             console.log(item.speciality);
             return item.speciality;
     }));
    const  DocSpecilist = Array.from(DocSpecilistSet)
-    console.log(DocSpecilist);
+    // console.log(DocSpecilist);
   return (
-    <div className='flex flex-col gap-4 text-sm text-gray-600'>{
+    <div >
+      <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter? 'bg-primary text-white ': ''}`} onClick={()=>setShowFilter(prev =>!prev)}>filter</button>
+      <div className={`flex flex-col gap-4 text-sm text-gray-600 ${showFilter ?'flex':'hidden sm:flex'}`}>{
        DocSpecilist.map((item,index)=>{
             console.log(item)
               return ( <p onClick={()=> navigate(`/doctors/${item}`)} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${item=== specialist ?"bg-indigo-100 text-black":""}`} key={"fth01"+index}>{item}</p>)
             })
        }
+       </div>
     </div>
   )
 }
