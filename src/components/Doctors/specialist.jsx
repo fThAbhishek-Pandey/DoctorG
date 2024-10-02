5966f9632362579677e48ff3d6f7bb1b7dbc2d05
fThAbhishek-Pandey <abhishek830564@email.com>
2024-10-02 19:44:35 +0530

    adding user profile page

--- src/components/Doctors/specialist.jsx
+++ src/components/Doctors/specialist.jsx
@@ -5,11 +5,17 @@ const Specialist = ({specialist}) => {
     const {doctors} = useContext(AppContext);
     const navigate = useNavigate();
     console.log(doctors);
+    const DocSpecilistSet = new  Set(doctors.map ((item)=>{
+            console.log(item.speciality);
+            return item.speciality;
+    }));
+   const  DocSpecilist = Array.from(DocSpecilistSet)
+    console.log(DocSpecilist);
   return (
     <div className='flex flex-col gap-4 text-sm text-gray-600'>{
-           doctors.map((item,index)=>{
+       DocSpecilist.map((item,index)=>{
             console.log(item)
-              return ( <p onClick={()=> navigate(`/doctors/${item.speciality}`)} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${item.speciality=== specialist ?"bg-indigo-100 text-black":""}`} key={"fth01"+index}>{item.speciality}</p>)
+              return ( <p onClick={()=> navigate(`/doctors/${item}`)} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${item=== specialist ?"bg-indigo-100 text-black":""}`} key={"fth01"+index}>{item}</p>)
             })
        }
     </div>
